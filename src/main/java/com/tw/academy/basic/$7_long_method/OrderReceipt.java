90998e60b99e65f49afaaf2a25c14cea76c7f7ed
LouisTang <louis.tang@cargosmart.com>
2021-06-22 21:53:04 +0800

    refactor: remove useless  comment

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -23,20 +23,14 @@ public class OrderReceipt {
         return o.getCustomerName();
     }
 
-    //todo: rename -- Tom
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        // print headers
         output.append(RECEIPT_HEADER);
 
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         output.append(o.getCustomerName());
         output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
 
-        // prints lineItems
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : o.getLineItems()) {
@@ -49,18 +43,14 @@ public class OrderReceipt {
             output.append(lineItem.totalAmount());
             output.append('\n');
 
-            // calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * TAX_RATE;
             totSalesTx += salesTax;
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
         }
 
-        // prints the state tax
         output.append(RECEIPT_SALES_TAX).append('\t').append(totSalesTx);
 
-        // print total amount
         output.append(RECEIPT_TOTAL_AMOUNT).append('\t').append(tot);
         return output.toString();
     }
